b99614cdbb62b3cb2d58e38b00c5c5c2cda85a00
Makoto Saito <saitomako@gmail.com>
2021-07-29 21:57:24 +0900

    add gas

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,13 +1,15 @@
-<script>$(function(){
-	$('.toggle_title').click(function(){
-		var clicks = $(this).data('clicks');
-		$(this).toggleClass('selected');
-		$(this).next().slideToggle();
-		if (clicks) {
-		} else {
-			var top = $(this).offset().top;
-			$('html,body').animate({scrollTop:top},500);
-		}
-		$(this).data("clicks", !clicks);
-	});
-});</script>
+<script>
+$(function(){
+  $('.toggle_title').click(function(){
+    var clicks = $(this).data('clicks');
+    $(this).toggleClass('selected');
+    $(this).next().slideToggle();
+    if (clicks) {
+    } else {
+      var top = $(this).offset().top;
+      $('html,body').animate({scrollTop:top},500);
+    }
+    $(this).data("clicks", !clicks);
+  });
+});
+</script>
